4b5124ab2390e497c42c619f1b76557013ffe6b5
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-06-03 10:08:27 -0700

    Add all files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+})
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 
 // Tags belongToMany Products (through ProductTag)
 
